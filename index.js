146489f8db6f8adf2a9875f37ed1da9c7c6fb407
BKB <blainekb94@gmail.com>
2022-10-07 02:58:50 -0500

    added inquirer prompts

--- index.js
+++ index.js
@@ -3,8 +3,61 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email?',
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of the project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'What is the description of your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How can your project be used?',
+    },
+    {
+      type: 'input',
+      name: 'features',
+      message: 'Can you please list important features of your project?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What is required to install your project?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What are the contribution guidelines for your project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the instructions to test your project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Which of the following licenses would you like to select for your project?',
+    },
+  ])
+};  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
