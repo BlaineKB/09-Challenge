011b58cf2bb366970d87adedf973e89191ae038d
BKB <blainekb94@gmail.com>
2022-10-08 17:44:33 -0500

    added functions to render the license link and license section for generated README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,11 +14,30 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return `## License
+    Details about the MIT license can be found here: [mit-license.org](https://mit-license.org/)`
+
+  } else if (license === "Apache 2.0") {
+    return `## License
+    Details about the Apache 2.0 license can be found here: [apache.org](https://www.apache.org/licenses/LICENSE-2.0.html)`
+
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "No License") {
+    return "";
+
+  } else {
+    return " [License](#license)"
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
